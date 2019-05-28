395a071105e87d06f580f5b93b1555907e5ec013
Scott Wilson <apollo@Scotts-MBP.attlocal.net>
2019-05-28 15:45:20 -0700

    first commit

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,18 +5,17 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const BlogPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
+    <SEO title="Blog" />
+    <h1>Hi this is blog</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/blog/">blog</Link>
   </Layout>
 )
 
-export default IndexPage
+export default BlogPage
